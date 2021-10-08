7e628a86f56119fa158e4d8d539c4b0854b8648c
anwarhossain1 <irishkhan33@gmail.com>
2021-10-08 21:38:45 +0600

    done

--- src/App.js
+++ src/App.js
@@ -4,16 +4,28 @@ import {
   increament,
   decreament,
   increase,
+  fetchData,
 } from "./redux/features/counterSlice";
 import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 
 function App() {
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchData());
+  }, []);
+
+  const statePosts = useSelector((state) => state.posts);
+  const { posts } = statePosts;
+  console.log(posts);
   // const stateData = useSelector((state) => state.counter);
   // console.log(stateData.value);
   return (
     <div className="App">
       <h1>Showing JSONPLACeholder as a practice</h1>
+      {posts.map((post) => {
+        return <h2>{post.title}</h2>;
+      })}
       {/* <h2>{stateData.value}</h2>
       <button onClick={() => dispatch(increament())}>+</button>
       <button onClick={() => dispatch(decreament())}>-</button>
